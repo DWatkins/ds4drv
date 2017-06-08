be52d202c408d0c880d558f9ce6e5179157d40ee
dwatkins <dexter.a.watkins@gmail.com>
2017-06-08 08:18:50 -0700

    updated socket address to loopback

--- ds4drv/logger.py
+++ ds4drv/logger.py
@@ -7,7 +7,7 @@ from threading import Lock
 LEVELS = ["none", "error", "warning", "info"]
 FORMAT = "[{level}][{module}] {msg}\n"
 
-host = '10.1.10.151'
+host = '127.0.0.1'
 port = 5555;
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
