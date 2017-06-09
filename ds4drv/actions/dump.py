30af97082f4636be2a9e09a1cc8245f9f194f043
root <root@beaglebone.localdomain>
2017-06-09 16:44:21 +0000

    changed dump frequency to 20hz

--- ds4drv/actions/dump.py
+++ ds4drv/actions/dump.py
@@ -9,7 +9,7 @@ class ReportActionDump(ReportAction):
 
     def __init__(self, *args, **kwargs):
         super(ReportActionDump, self).__init__(*args, **kwargs)
-        self.timer = self.create_timer(0.5, self.dump)
+        self.timer = self.create_timer(0.05, self.dump)
 
     def enable(self):
         self.timer.start()
